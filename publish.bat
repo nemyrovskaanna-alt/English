608ccdad0ab3766468704d78821ea646c13f692f
nemyrovskaanna-alt <nemyrovskaanna@gmail.com>
2026-08-03 14:19:55 +0300

    Fix root deployment

--- publish.bat
+++ publish.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal
 git add present-simple.html
-git commit -m "Add Present Simple test"
-if errorlevel 1 exit /b %errorlevel%
+git diff --staged --quiet
+if errorlevel 1 git commit -m "Add Present Simple test"
 git push origin main
